459adcf3d955065e7021ce644af10af66f13c682
Alfonso <alfewhite@gmail.com>
2018-10-15 09:14:27 +0100

    Added no-duplicates champion code, color switching and name fields - name submit button and status text to go, then op.gg renewal & profiel download code

--- public/index.js
+++ public/index.js
@@ -2,18 +2,27 @@
 
 $( function() {
 
+    var i = 0;
+
     // Import champion data
     $.getJSON( "champ_list.json", function( data ) {
-        var champ_list = data;
 
         // Constants
+        var champ_list = data; // Riot champion data
+
+        var ch_cid_li = 0; // Indexes of each feature in champ_list
+        var ch_id_li = 1;
+        var ch_name_li = 2;
+
+        // Create dictionary from indexes to cids
+        var champ_ind_dict = {};
+        for (i = 0; i < champ_list.length; i++) {
+            champ_ind_dict[champ_list[i][ch_cid_li]] = i;
+        }
+
         var ddrag_ver = "8.20.1";
         var ddrag_url = "https://ddragon.leagueoflegends.com/cdn/" + ddrag_ver + "/img/champion/";
         var none_champ_img = "imgs/searchicon.png";
-        var team_li = 0;
-        var role_li = 1;
-        var cid_li = 2;
-        var opgg_li = 3;
         var roles_ord = ['Top', 'Jungle', 'Middle', 'Support', 'Bottom'];
 
         // Champion grid element
@@ -21,7 +30,7 @@ $( function() {
         for (i = 0; i < champ_list.length; i++) {
             var img_url = none_champ_img;
             if (i > 0) {
-                img_url = ddrag_url + champ_list[i][1] + '.png';
+                img_url = ddrag_url + champ_list[i][ch_id_li] + '.png';
             }
             grid_content += '<img src="' + img_url + '" height="60" width="60" class="grid_img" id="grid_img_' + i + '">';
         }
@@ -42,6 +51,11 @@ $( function() {
             [ 1, 4, -1, -1 ],
         ];
 
+        var team_li = 0; // Indexes of each feature in req_data
+        var role_li = 1;
+        var cid_li = 2;
+        var opgg_li = 3;
+
         var app_height = $( "#app_area" ).height();
         var app_width = $( "#app_area" ).width();
 
@@ -53,7 +67,6 @@ $( function() {
         //     '<input type="submit" value="Submit Comment" /></form>'
         var pl_inner = '<div class="champion_buttons">' + 
             '<button class="search_button">s</button>' +
-            '' +
             '<button class="grid_button">g</button></div>' + 
             '<div class="champion_box"></div>' + 
             '<div class="role_cont"><select class="role_select">';
@@ -64,7 +77,9 @@ $( function() {
             l_roles[i] = i;
             r_roles[i] = i + 5;
         }
-        pl_inner += '</select></div>';
+        pl_inner += '</select></div>' +
+            '<div class="name_cont"><input type="text" placeholder="[unknown]" class="name_input"></input></div>' +
+            '';
 
         // Create left side player objects, and all player placeholders
         for (i = 0; i < 5; i++) {
@@ -125,7 +140,7 @@ $( function() {
             '<select data-placeholder="None (Unknown)" class="chosen-select search_select" tabindex="1">';
         for (i = 0; i < champ_list.length; i++) {
             ch = champ_list[i];
-            search_dropdown_inner += '<option value="' + ch[0] + '">' + ch[2] + '</option>';
+            search_dropdown_inner += '<option value="' + ch[ch_cid_li] + '">' + ch[ch_name_li] + '</option>';
         }
         search_dropdown_inner += '</select></div>';
             // '<option value="-1">None (Unknown)</option>' +
@@ -136,7 +151,7 @@ $( function() {
         $( "#pl_area" ).append(search_dropdown_inner);
 
         var ch_pl_i = 0; // Current pl index for which we are selecting a champion
-        var selected_indices = [...Array(10).keys()].map(i => 0); // Selected indices for each pl
+        var selected_ch_indices = [...Array(10).keys()].map(i => 0); // Selected champion list indices for each pl
 
         // Define champion grid triggers
         $( ".grid_button" ).click(function() { // Make grid appear
@@ -149,7 +164,7 @@ $( function() {
             ch_pl_i = parseInt(pl_id[pl_id.length - 1]);
 
             // Highlight the currently selected champion in the grid
-            current_i = selected_indices[ch_pl_i];
+            current_i = selected_ch_indices[ch_pl_i];
             curr_img_id = 'grid_img_' + current_i;
             $( "#" + curr_img_id ).addClass("grid_img_selected");
         });
@@ -157,7 +172,7 @@ $( function() {
             $(this).toggleClass("show");
             $(this).toggleClass("hide");
 
-            var current_i = selected_indices[ch_pl_i];
+            var current_i = selected_ch_indices[ch_pl_i];
             var curr_img_id = 'grid_img_' + current_i;
             $( "#" + curr_img_id ).removeClass("grid_img_selected");
         });
@@ -166,15 +181,36 @@ $( function() {
             $("#champion_grid_cont").toggleClass("show");
             $("#champion_grid_cont").toggleClass("hide");
 
-            var current_i = selected_indices[ch_pl_i];
+            var current_i = selected_ch_indices[ch_pl_i];
 
             var sel_ind = $(this).attr("id").split('_');
             sel_ind = parseInt(sel_ind[sel_ind.length - 1]);
-            selected_indices[ch_pl_i] = sel_ind;
-            req_data[ch_pl_i][cid_li] = champ_list[sel_ind][0];
+            selected_ch_indices[ch_pl_i] = sel_ind;
+            var new_cid = champ_list[sel_ind][ch_cid_li];
+            var old_cid = req_data[ch_pl_i][cid_li];
+            req_data[ch_pl_i][cid_li] = new_cid;
             if (sel_ind > 0) {
+                // If there is another entry with the same champion, switch champions with that player
+                for (i = 0; i < 10; i++) {
+                    if (i == ch_pl_i) {
+                        continue;
+                    }
+                    if (req_data[i][cid_li] == new_cid) {
+                        req_data[i][cid_li] = old_cid;
+                        old_sel_ind = champ_ind_dict[old_cid];
+                        selected_ch_indices[i] = old_sel_ind;
+                        if (old_cid != -1) {
+                            $( '#pl_' + i ).find( '.champion_box' ).css(
+                                "background-image", 'url(' + ddrag_url + champ_list[old_sel_ind][ch_id_li] + '.png)');
+                        } else {
+                            $( '#pl_' + i ).find( '.champion_box' ).css(
+                                "background-image", 'url(' + none_champ_img + ')');
+                        }
+                        break;
+                    }
+                }
                 $( '#pl_' + ch_pl_i ).find( '.champion_box' ).css(
-                    "background-image", 'url(' + ddrag_url + champ_list[sel_ind][1] + '.png)');
+                    "background-image", 'url(' + ddrag_url + champ_list[sel_ind][ch_id_li] + '.png)');
             } else {
                 $( '#pl_' + ch_pl_i ).find( '.champion_box' ).css(
                     "background-image", 'url(' + none_champ_img + ')');
@@ -182,6 +218,23 @@ $( function() {
 
             var curr_img_id = 'grid_img_' + current_i;
             $( "#" + curr_img_id ).removeClass("grid_img_selected");
+        });
+
+        // Define team colour switching
+        var left_col = "Blue";
+        var left_text_col = "blue";
+        var right_col = "Red";
+        var right_text_col = "red";
+        $( "#switch_cols_button" ).click(function() {
+            left_col = [right_col, right_col = left_col][0]; // Swap variable values (pre-ES6 compatible)
+            left_text_col = [right_text_col, right_text_col = left_text_col][0];
+            $( "#l_team_col" ).html(left_col);
+            $( "#l_team_col" ).css("color", left_text_col);
+            $( "#r_team_col" ).html(right_col);
+            $( "#r_team_col" ).css("color", right_text_col);
+            for (i = 0; i < 10; i++) {
+                req_data[i][0] = 1 - req_data[i][0];
+            }
         });
 
         // Define champion search show/hide trigger & positioning
@@ -195,7 +248,7 @@ $( function() {
 
                 // Set the current pl_i for editing the selected champion
                 ch_pl_i = parseInt(pl_id[pl_id.length - 1]);
-                $( ".search_select" )[0].selectedIndex = selected_indices[ch_pl_i];
+                $( ".search_select" )[0].selectedIndex = selected_ch_indices[ch_pl_i];
                 $( ".search_select" ).trigger('chosen:updated');
                 pos_x = parseInt($( '#' + pl_id ).css('left')) + 30;
                 pos_y = parseInt($( '#' + pl_id ).css('top')) + pl_height - 5;
@@ -219,11 +272,32 @@ $( function() {
 
             // At this point, set the new champion id & image
             var sel_ind = $(this)[0].selectedIndex;
-            selected_indices[ch_pl_i] = sel_ind;
-            req_data[ch_pl_i][cid_li] = champ_list[sel_ind][0];
+            selected_ch_indices[ch_pl_i] = sel_ind;
+            var new_cid = champ_list[sel_ind][ch_cid_li];
+            var old_cid = req_data[ch_pl_i][cid_li];
+            req_data[ch_pl_i][cid_li] = new_cid;
             if (sel_ind > 0) {
+                // If there is another entry with the same champion, switch champions with that player
+                for (i = 0; i < 10; i++) {
+                    if (i == ch_pl_i) {
+                        continue;
+                    }
+                    if (req_data[i][cid_li] == new_cid) {
+                        req_data[i][cid_li] = old_cid;
+                        old_sel_ind = champ_ind_dict[old_cid];
+                        selected_ch_indices[i] = old_sel_ind;
+                        if (old_cid != -1) {
+                            $( '#pl_' + i ).find( '.champion_box' ).css(
+                                "background-image", 'url(' + ddrag_url + champ_list[old_sel_ind][ch_id_li] + '.png)');
+                        } else {
+                            $( '#pl_' + i ).find( '.champion_box' ).css(
+                                "background-image", 'url(' + none_champ_img + ')');
+                        }
+                        break;
+                    }
+                }
                 $( '#pl_' + ch_pl_i ).find( '.champion_box' ).css(
-                    "background-image", 'url(' + ddrag_url + champ_list[sel_ind][1] + '.png)');
+                    "background-image", 'url(' + ddrag_url + champ_list[sel_ind][ch_id_li] + '.png)');
             } else {
                 $( '#pl_' + ch_pl_i ).find( '.champion_box' ).css(
                     "background-image", 'url(' + none_champ_img + ')');
