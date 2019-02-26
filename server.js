05cfd46d611f45c5334b9b1a623387c33dd27903
Alfonso <alfewhite@gmail.com>
2019-02-26 23:05:01 +0000

    Updated for new repo format

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const amqp = require('amqplib/callback_api')
 
 
 // Script options
-const port = 32777 // Port to listen on for match prediction POST requests
-// const port = 8080
+// const port = 32777 // Port to listen on for match prediction POST requests
+const port = 8080
 const rabbit_host = 'localhost' // RabbitMQ host IP
 // const rabbit_host = '18.130.123.231' // RabbitMQ host IP
 const rmq_user = "alfy"
